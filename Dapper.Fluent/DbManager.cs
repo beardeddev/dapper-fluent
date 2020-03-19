fa0db5143e3438efbc5c7da0820384df32ee80a1
beardeddev <beardeddev@gmail.com>
2020-03-19 16:07:32 +0200

    implement async DbManager

--- Dapper.Fluent/DbManager.cs
+++ Dapper.Fluent/DbManager.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Collections;
 using System.Data.Common;
 using static Dapper.SqlMapper;
+using System.Threading.Tasks;
 
 namespace Dapper.Fluent
 {
@@ -316,6 +317,131 @@ namespace Dapper.Fluent
         {
             return this.DbConnection.Execute(this.commandText, this.parameters, this.Transaction, this.commandTimeout, this.commandType);
         }
+
+        /// <summary>Executes SQL statement on the database and returns a collection of objects.</summary>
+        /// <typeparam name="T">The type of the elements in the returned collection.</typeparam>
+        /// <returns>A collection of objects returned by the query.</returns>
+        public Task<IEnumerable<T>> ExecuteListAsync<T>()
+        {
+            return this.DbConnection.QueryAsync<T>(this.commandText, this.parameters, this.Transaction, this.commandTimeout, this.commandType);
+        }
+
+        /// <summary>Executes SQL statement against the connection and returns the result.</summary>
+        /// <typeparam name="T">The type of the element to be the returned.</typeparam>
+        /// <returns>An object returned by the query.</returns>
+        public Task<T> ExecuteObjectAsync<T>()
+        {
+            return this.DbConnection.QueryFirstOrDefaultAsync<T>(this.commandText, this.parameters, this.Transaction, this.commandTimeout, this.commandType);
+        }
+
+        /// <summary>Executes SQL statement against the connection and builds multiple result sets.</summary>
+        /// <typeparam name="T1">The type of the first result set elements to be the returned.</typeparam>
+        /// <typeparam name="T2">The type of the second result set elements to be the returned.</typeparam>
+        /// <returns>A <see cref="System.Tuple<T1, T2>">System.Tuple&lt;T1, T2&gt;</see> object of multiple result sets returned by the query.</returns>
+        public async Task<Tuple<IEnumerable<T1>, IEnumerable<T2>>> ExecuteMultipleAsync<T1, T2>()
+        {
+            using (var rs = await this.DbConnection.QueryMultipleAsync(commandText, this.parameters, this.Transaction, this.commandTimeout, this.commandType))
+            {
+                IEnumerable<T1> item1 = rs.Read<T1>();
+                IEnumerable<T2> item2 = rs.Read<T2>();
+
+                return new Tuple<IEnumerable<T1>, IEnumerable<T2>>(item1, item2);
+            }
+        }
+
+        /// <summary>Executes SQL statement against the connection and builds multiple result sets.</summary>
+        /// <typeparam name="T1">The type of the first result set elements to be the returned.</typeparam>
+        /// <typeparam name="T2">The type of the second result set elements to be the returned.</typeparam>
+        /// <typeparam name="T3">The type of the third result set elements to be the returned.</typeparam>
+        /// <returns>A <see cref="System.Tuple<T1, T2, T3>">System.Tuple&lt;T1, T2, T3&gt;</see> object of multiple result sets returned by the query.</returns>
+        public async Task<Tuple<IEnumerable<T1>, IEnumerable<T2>, IEnumerable<T3>>> ExecuteMultipleAsync<T1, T2, T3>()
+        {
+            using (var rs = await this.DbConnection.QueryMultipleAsync(commandText, this.parameters, this.Transaction, this.commandTimeout, this.commandType))
+            {
+                IEnumerable<T1> item1 = rs.Read<T1>();
+                IEnumerable<T2> item2 = rs.Read<T2>();
+                IEnumerable<T3> item3 = rs.Read<T3>();
+
+                return new Tuple<IEnumerable<T1>, IEnumerable<T2>, IEnumerable<T3>>(item1, item2, item3);
+            }
+        }
+
+        /// <summary>Executes SQL statement against the connection and maps a single row to multiple objects.</summary>
+        /// <typeparam name="T1">The type of the first result set.</typeparam>
+        /// <typeparam name="T2">The type of the second result set.</typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="map">The mapping function that encapsulates a method that has three parameters and returns a value of the type specified by the TResult parameter..</param>
+        /// <param name="splitOn">The name of the field result set should split and read the second object from (default: id).</param>
+        /// <returns>The result object with related associations.</returns>
+        public Task<IEnumerable<TResult>> ExecuteMappingAsync<T1, T2, TResult>(Func<T1, T2, TResult> map, string splitOn = "Id")
+        {
+            return this.DbConnection.QueryAsync<T1, T2, TResult>(this.commandText, map, this.parameters, this.Transaction, this.buffered, splitOn, this.commandTimeout, this.commandType);
+        }
+
+        /// <summary>Executes SQL statement against the connection and maps a single row to multiple objects.</summary>
+        /// <typeparam name="T1">The type of the first result set.</typeparam>
+        /// <typeparam name="T2">The type of the second result set.</typeparam>
+        /// <typeparam name="T3">The type of the third result set.</typeparam>
+        /// <typeparam name="TResult">The type of the result set elements to be the returned.</typeparam>
+        /// <param name="map">The mapping function that encapsulates a method that has three parameters and returns a value of the type specified by the TResult parameter..</param>
+        /// <param name="splitOn">The name of the field result set should split and read the second object from (default: id).</param>
+        /// <returns>The result object with related associations.</returns>
+        public Task<IEnumerable<TResult>> ExecuteMappingAsync<T1, T2, T3, TResult>(Func<T1, T2, T3, TResult> map, string splitOn = "Id")
+        {
+            return this.DbConnection.QueryAsync<T1, T2, T3, TResult>(this.commandText, map, this.parameters, this.Transaction, this.buffered, splitOn, this.commandTimeout, this.commandType);
+        }
+
+        /// <summary>Executes SQL statement against the connection and maps a single row to multiple objects.</summary>
+        /// <typeparam name="T1">The type of the first result set.</typeparam>
+        /// <typeparam name="T2">The type of the second result set.</typeparam>
+        /// <typeparam name="T3">The type of the third result set.</typeparam>
+        /// <typeparam name="T4">The type of the fourth result set.</typeparam>
+        /// <typeparam name="TResult">The type of the result set elements to be the returned.</typeparam>
+        /// <param name="map">The mapping function that encapsulates a method that has three parameters and returns a value of the type specified by the TResult parameter..</param>
+        /// <param name="splitOn">The name of the field result set should split and read the second object from (default: id).</param>
+        /// <returns>The result object with related associations.</returns>
+        public Task<IEnumerable<TResult>> ExecuteMappingSync<T1, T2, T3, T4, TResult>(Func<T1, T2, T3, T4, TResult> map, string splitOn = "Id")
+        {
+            return this.DbConnection.QueryAsync<T1, T2, T3, T4, TResult>(this.commandText, map, this.parameters, this.Transaction, this.buffered, splitOn, this.commandTimeout, this.commandType);
+        }
+
+        /// <summary>Executes SQL statement against the connection and maps a single row to multiple objects.</summary>
+        /// <typeparam name="T1">The type of the first result set.</typeparam>
+        /// <typeparam name="T2">The type of the second result set.</typeparam>
+        /// <typeparam name="T3">The type of the third result set.</typeparam>
+        /// <typeparam name="T4">The type of the fourth result set.</typeparam>
+        /// <typeparam name="T5">The type of the fifth result set.</typeparam>
+        /// <typeparam name="TResult">The type of the result set elements to be the returned.</typeparam>
+        /// <param name="map">The mapping function that encapsulates a method that has three parameters and returns a value of the type specified by the TResult parameter..</param>
+        /// <param name="splitOn">The name of the field result set should split and read the second object from (default: id).</param>
+        /// <returns>The result object with related associations.</returns>
+        public Task<IEnumerable<TResult>> ExecuteMappinAsync<T1, T2, T3, T4, T5, TResult>(Func<T1, T2, T3, T4, T5, TResult> map, string splitOn = "Id")
+        {
+            return this.DbConnection.QueryAsync<T1, T2, T3, T4, T5, TResult>(this.commandText, map, this.parameters, this.Transaction, this.buffered, splitOn, this.commandTimeout, this.commandType);
+        }
+
+        /// <summary>Executes SQL statement against the connection and maps a single row to multiple objects.</summary>
+        /// <typeparam name="T1">The type of the first result set.</typeparam>
+        /// <typeparam name="T2">The type of the second result set.</typeparam>
+        /// <typeparam name="T3">The type of the third result set.</typeparam>
+        /// <typeparam name="T4">The type of the fourth result set.</typeparam>
+        /// <typeparam name="T5">The type of the fifth result set.</typeparam>
+        /// <typeparam name="T6">The type of the sixth result set.</typeparam>
+        /// <typeparam name="TResult">The type of the result set elements to be the returned.</typeparam>
+        /// <param name="map">The mapping function that encapsulates a method that has three parameters and returns a value of the type specified by the TResult parameter..</param>
+        /// <param name="splitOn">The name of the field result set should split and read the second object from (default: id).</param>
+        /// <returns>The result object with related associations.</returns>
+        public Task<IEnumerable<TResult>> ExecuteMappingAsync<T1, T2, T3, T4, T5, T6, TResult>(Func<T1, T2, T3, T4, T5, T6, TResult> map, string splitOn)
+        {
+            return this.DbConnection.QueryAsync<T1, T2, T3, T4, T5, T6, TResult>(this.commandText, map, this.parameters, this.Transaction, this.buffered, splitOn, this.commandTimeout, this.commandType);
+        }
+
+        /// <summary>Executes a SQL statement against the connection and returns the number of rows affected.</summary>
+        /// <returns>The number of rows affected.</returns>
+        public Task<int> ExecuteAsync()
+        {
+            return this.DbConnection.ExecuteAsync(this.commandText, this.parameters, this.Transaction, this.commandTimeout, this.commandType);
+        }
         #endregion
 
         #region Fluent API
@@ -443,7 +569,7 @@ namespace Dapper.Fluent
         {
             this.parameters.AddDynamicParams(parameters);
             return this.SetSpCommand(commandText);
-        }
+        }        
         #endregion
     }
 }
